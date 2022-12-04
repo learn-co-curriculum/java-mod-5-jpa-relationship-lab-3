ce71caff508f0b2abf2d20fd46e730c515c7b9b3
Linda Seiter <linseiter@gmail.com>
2022-12-04 13:21:03 -0500

    update postgres, add starter source code

---
+++ src/main/java/org/example/model/Cat.java
@@ -0,0 +1,68 @@
+package org.example.model;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cat {
+
+    private int id;
+
+    private String name;
+
+    private int age;
+
+    private String breed;
+
+    private List<Owner> owners = new ArrayList<>();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public List<Owner> getOwners() {
+        return owners;
+    }
+
+    public void addOwner(Owner owner) {
+        owners.add(owner);
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", breed='" + breed + '\'' +
+                '}';
+    }
+}
